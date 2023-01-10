b453f3d80f81286fe9f40fae8474f99f0c8b57d4
the-alchemist-gh <malikbanks01@gmail.com>
2023-01-10 14:26:03 +0000

    Fix: geogating 34

--- pages/about-2.js
+++ pages/about-2.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import AboutPill from '../components/AboutPage'
+import AboutPill from '../Components/AboutPage'
 
 // import state from '../components/state'
 // import { useSnapshot } from 'valtio'
